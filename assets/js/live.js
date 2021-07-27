94da817d03d13baf2cb3d18ed9a8b1c0597ca298
Ken <s1110434045@gms.nutc.edu.tw>
2021-07-27 19:29:05 +0800

    live page test

--- assets/js/live.js
+++ assets/js/live.js
@@ -1,3 +1,14 @@
+function open_hide_menu() {
+    $(".bottom-hide-tool-block").show();
+}
+
+$(document).mouseup(function(e) {
+    var container = $(".bottom-hide-tool-block"); // 這邊放你想要排除的區塊
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+        $(".bottom-hide-tool-block").hide();
+    }
+});
+
 $(function() {
     // 定義要取得的影音內容，包含影像和聲音
     let constraints = {
